7695214931f30aa1a3fa18afaa75d1963106bd30
Michael Beigelmacher <michael.beigelmacher@posit.co>
2024-06-29 18:16:14 -0400

    print the value of api_url

--- backend/rxconfig.py
+++ backend/rxconfig.py
@@ -10,6 +10,8 @@ if os.getenv("RSTUDIO_PRODUCT") == "CONNECT":
         os.environ["CONNECT_SERVER"], f"content/{os.environ["CONNECT_CONTENT_GUID"]}"
     )
 
+print(f"{api_url=}")
+
 config = rx.Config(
     app_name="intro_reflex",
     api_url=api_url,
